10f243071dd44502c5ed5a295581f777063f37ca
testautomation <testautomation052018@wp.pl>
2018-05-21 15:50:07 +0200

    First test done. Pom.xml created.

--- qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
+++ qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
@@ -8,6 +8,9 @@ public class JunitTest {
 
     @Test
     public void firstTest() {
+        String myString = "First Test";
+
         assertTrue (true==true);
+        assertTrue(myString.equals("First Test"));
     }
 }
